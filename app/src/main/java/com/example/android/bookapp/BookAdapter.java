648619404f63eb9a6304441ff88286564d302f26
Mateusz <wszeborowskimateusz@gmail.com>
2017-09-17 12:09:31 +0200

    Adding EditView for searching and the search button

--- app/src/main/java/com/example/android/bookapp/BookAdapter.java
+++ app/src/main/java/com/example/android/bookapp/BookAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.bookapp;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -19,7 +18,7 @@ import java.util.List;
 
 public class BookAdapter extends ArrayAdapter<Book> {
 
-    public BookAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Book> objects) {
+    public BookAdapter(@NonNull Context context, @NonNull List<Book> objects) {
         super(context,0, objects);
     }
 
--- app/src/main/java/com/example/android/bookapp/MainActivity.java
+++ app/src/main/java/com/example/android/bookapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.bookapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Kubus Puchatek","Jan Brzechwa","Bardzo fajna ksiazka z wieloma zwrotami akcji.Bardzo fajna ksiazka z wieloma zwrotami akcji"));
+        books.add(new Book("Kubus Prawdziwek","Andrzej Wajda","Ksiazka taka srednia"));
+        books.add(new Book("Marsz mieczy przez pustkowie","Nicol Kidman","Niby ksiazka niby nie"));
+        books.add(new Book("Kubus Prawdziwek","Andrzej Wajda","Ksiazka taka srednia"));
+        books.add(new Book("Marsz mieczy przez pustkowie","Nicol Kidman","Niby ksiazka niby nie"));
+
+        BookAdapter adapt = new BookAdapter(this,books);
+
+        ListView view = (ListView) findViewById(R.id.books_list);
+        view.setAdapter(adapt);
     }
 }
